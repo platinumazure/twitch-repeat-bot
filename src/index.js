e204fb46721c52d1381186bd8e32e7c1b8828d4e
Kevin Partington <platinumazure@gmail.com>
2022-01-12 20:07:16 -0600

    feat: Add platinumazure identity and test sending messages to the chat

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const tmi = require("tmi.js");
 
 const client = new tmi.Client({
+    identity: {
+        username: "platinumazure",
+        password: process.env.TWITCH_REPEAT_BOT_SECRET
+    },
     channels: ["platinumazure"]
 });
 
 client.connect();
 
-client.on("message", (channel, tags, message) => {
+client.on("message", (channel, tags, message, self) => {
+    if (self) { return; }
+
     // "Alca: Hello, World!"
     console.log(`${tags["display-name"]}: ${message}`);
+
+    if (message.toLowerCase().startsWith("!repeat")) {
+        client.say(channel, `Hi @${tags.username}, message received!`);
+    }
 });
+
+console.log("twitch-repeat-bot: Successfully initialized");
